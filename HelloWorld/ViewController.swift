e95c9599b94fd2dd5559cf89a877018528f2df9f
takahashi-shinhi-002 <switchscratch015@gmail.com>
2019-12-13 08:09:03 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 高橋信次 on 2019/12/13.
+//  Copyright © 2019 高橋信次. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
